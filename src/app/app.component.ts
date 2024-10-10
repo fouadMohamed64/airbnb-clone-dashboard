60076d37f8971990feee011ee52a78327b359b25
fouadMohamed <fm5669653@gmail.com>
2024-10-10 16:51:39 +0300

    create header footer sidebar components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./Components/sidebar/sidebar.component";
+import { FooterComponent } from "./Components/footer/footer.component";
+import { HeaderComponent } from "./Components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
